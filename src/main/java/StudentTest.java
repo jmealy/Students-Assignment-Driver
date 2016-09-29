4564f459e92c0006a9930fb3b7f238d4eaee8243
James Mealy <jamesmealy1@gmail.com>
2016-09-29 18:55:06 +0100

    added .jar file

--- src/main/java/StudentTest.java
+++ src/main/java/StudentTest.java
@@ -1,6 +1,16 @@
+import org.joda.time.DateTime;
 
 public class StudentTest {
 	
-	//Module m1 = new Module(null, 0);
+	public static void main(String args[]){
+		
+		Course c1 = new Course("ECE", new DateTime(2016,1,1,0,0), new DateTime(2016,5,5,0,0) );
+		Module m1 = new Module("Machine Learning", "CT111" );
+		Module m2 = new Module("Software Engineering", "CT222");
+		Student s1 = new Student("J. Murphy", 1990, 1234123);
+		Student s2 = new Student("P. Malanophy", 1992, 1334123);
+		
+		
+	}
 
 }
